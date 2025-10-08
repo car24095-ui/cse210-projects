6150fc020b19f8c2e2f0009849aa6972dc0ceec1
Jonathan <car24095@byui.edu>
2025-10-08 15:39:22 -0500

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magic = new Random();
+        int number = magic.Next(1, 101);
+
+        int guess = 101;
+        while (guess != number)
+        {
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+        
+        Console.WriteLine("You guessed it!");
+
     }
 }
